555d2c380bd6afd5f3c75036567d25f5a4b3f870
Kennie-Simz <nyongesa17@outlook.com>
2022-07-18 13:16:00 -0700

    countess

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -49,7 +49,7 @@ void counting_sort(int *array, size_t size)
 	for (i = 0; i < size; i++)
 	{
 		array[count[new[i]] - 1] = new[i];
-		ount[new[i]]--;
+		count[new[i]]--;
 	}
 	free(count);
 	free(new);
